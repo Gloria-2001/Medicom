41eed4d174c839c6cc015f28ab3da92fdf936991
Gloria-2001 <olimen141@gmail.com>
2021-06-02 11:36:57 -0500

    PWA

--- Pagina General/sw.js
+++ Pagina General/sw.js
@@ -1,4 +1,4 @@
-const staticCacheName='site-static-v1';
+const staticCacheName='site-static-v3';
 const dynamicCacheName='site-dynamic-v1';
 const assets=[
     './',
@@ -46,6 +46,17 @@ const assets=[
     './fallback.html'
 ];
 
+//cache size limit function
+const limitCacheSize = (name,size)=>{
+    caches.open(name).then(cache=>{
+        cache.keys().then(keys=>{
+            if(keys.length>size){
+                cache.delete(keys[0]).then(limitCacheSize(name,size));
+            }
+        })
+    })
+};
+
 //install service worker
 self.addEventListener("install",(evt)=>{
     //console.log("Service worker install");
@@ -64,7 +75,7 @@ self.addEventListener("activate",(evt)=>{
         caches.keys().then(keys=>{
             //console.log(keys);
             return Promise.all(keys
-                .filter(key => key!==staticCacheName)    
+                .filter(key => key!==staticCacheName && key!==dynamicCacheName)    
                 .map(key=>caches.delete(key))
             )
         })
@@ -79,9 +90,14 @@ self.addEventListener("fetch",(evt)=>{
             return cacheRes || fetch(evt.request).then(fetchRes=>{
                 return caches.open(dynamicCacheName).then(cache=>{
                     cache.put(evt.request.url,fetchRes.clone());
+                    limitCacheSize(dynamicCacheName,50)
                     return fetchRes;
                 })
             });
+        }).catch(()=> {
+            if(evt.request.url.indexOf('.html') > -1){
+                return caches.match('./fallback.html');
+            }
         })
-    )
-})
+    );
+});
