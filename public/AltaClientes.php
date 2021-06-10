dabddc4e5732bd4feac6341031f820913b30ff81
pablo-morales-01 <pmoralesz1600@alumno.ipn.mx>
2021-06-10 01:45:50 -0500

    Conexion a BD

--- public/AltaClientes.php
+++ public/AltaClientes.php
@@ -1,42 +1,62 @@
 <?php
-$hostname="localhost";
-$database="id16940318_medicom";
-$username="id16940318_medicom123";
-$password="z1$RLFCg@aqf}U<h";
-$json=array();
-	if(isset($_GET["USER"]) && isset($_GET["contrasena"])){
-		$USER=$_GET['USER'];
-		$contrasena=$_GET['contrasena'];
+   include ("conexion.php");
 
-		$conexion=mysqli_connect($hostname,$username,$password,$database);
+   
+   $alergias=$_POST['alergias'];
+   
+   if (strcmp($alergias, "No")==0) {
+      $Alergia="Ninguna";
 
-		$consulta="INSERT INTO login(id_Paciente,contrasena) VALUES ('{$USER}','{$contrasena}')";
-		$resultado=mysqli_query($conexion,$consulta);
-
-		if($consulta){
-
-			$consulta="SELECT * FROM login WHERE id_Paciente='{$USER}'";
-		   	$resultado=mysqli_query($conexion,$consulta);
-
-			if($reg=mysqli_fetch_array($resultado)){
-				$json['datos'][]=$reg;
-			}
-			mysqli_close($conexion);
-			echo json_encode($json);
-		}
-
-		else{
-         $results["USER"]='';
-			$results["contrasena"]='';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
-	}
-
-	else{
-         $results["USER"]='';
-         $results["contrasena"]='';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
+   }
+   else{
+      $Alergia=$_POST['alergia'];
+   }
+   $tabla2="login";
+   $tabla1="paciente";
+   $Pass=$_POST['contrasena'];
+   $Pass1=$_POST['contrasena1'];
+   if (strcmp($Pass,$Pass1)==0) {
+      echo"<script>
+    alert('Las contraseñas  coinciden');
+    </script>";
+    if($_POST)
+      {
+         $queryInsert1 = "INSERT INTO $tabla2 (id_Paciente, contrasena) VALUES ('".$_POST['USER']."', '".$_POST['contrasena']."');";
+ 
+         $resultInsert1 = mysqli_query($conn, $queryInsert1); 
+ 
+         if($resultInsert1)
+         {
+            echo "<strong>Se ingresaron los registros con exito login</strong>. <br>";
+         }
+         else
+         {
+            echo "No se ingresaron los registros.log <br>";
+         }
+ 
+      }
+      if($_POST)
+      {
+         $queryInsert = "INSERT INTO $tabla1 (id_Paciente,Nombre_Paciente,Apellido_P,Apellido_M,direccion,CP,sexo,CURP,NSS,edo_civil,Peso,Estatura,Alergias,Tel_Paciente,correo,Fecha_Nac) VALUES ('".$_POST['USER']."','".$_POST['Nombre']."','".$_POST['Apellido_P']."','".$_POST['Apellido_M']."','".$_POST['Calle'].",".$_POST['Num'].",".$_POST['Col'].",".$_POST['Del']."','".$_POST['Cp']."','".$_POST['sexo']."','".$_POST['CURP']."','".$_POST['NSS']."','".$_POST['edoCivil']."','".$_POST['Peso']."','".$_POST['Talla']."','".$_POST['alergias']."','".$_POST['Tel']."','".$_POST['email']."','".$_POST['anio']."-".$_POST['mes']."-".$_POST['dia']."');";
+ 
+         $resultInsert = mysqli_query($conn, $queryInsert); 
+ 
+         if($resultInsert)
+         {
+            echo "<strong>Se ingresaron los registros con exito paciente</strong>. <br>";
+         }
+         else
+         {
+            echo "No se ingresaron los registros.pac <br>";
+         }
+ 
+      }
+   }else{
+      echo"<script>
+    alert('Las contraseñas no coinciden');
+    </script>";
+    }
+   
+          
+mysqli_close($conn);
 ?>
--- public/conexion.php
+++ public/conexion.php
@@ -1,39 +1,14 @@
 <?php
-$hostname="localhost";
-$database="id16940318_medicom";
-$username="id16940318_medicom123";
-$password="z1$RLFCg@aqf}U<h";
-$json=array();
-	if(isset($_GET["USER"]) && isset($_GET["contrasena"])){
-		$USER=$_GET['USER'];
-		$contrasena=$_GET['contrasena'];
-
-		$conexion=mysqli_connect($hostname,$username,$password,$database);
-
-		$consulta="SELECT id_Paciente, contrasena FROM login WHERE id_Paciente = '{$USER}' AND contrasena = '{$contrasena}'";
-		$resultado=mysqli_query($conexion,$consulta);
-
-		if($consulta){
-
-			if($reg=mysqli_fetch_array($resultado)){
-				$json['datos'][]=$reg;
-			}
-			mysqli_close($conexion);
-			echo json_encode($json);
-		}
-
-		else{
-			$results["USER"]='';
-			$results["contrasena"]='';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
-	}
-
-	else{
-      $results["USER"]='';
-      $results["contrasena"]='';
-			$json['datos'][]=$results;
-			echo json_encode($json);
-		}
+$servername = "localhost";
+$database = "id16940318_medicom";
+$username = "id16940318_medicomdb";
+$password = "G-12e-34R-56";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+//mysqli_close($conn);
 ?>
