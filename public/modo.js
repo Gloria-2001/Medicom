b88c43856f8c7da5a1595179f19e030086d96f7b
Gloria-2001 <olimen141@gmail.com>
2021-06-13 00:32:07 -0500

    Ajustes pagina medico y modal

--- public/modo.js
+++ public/modo.js
@@ -1,13 +1,30 @@
-function myFunction() {
-  // Get the checkbox
-  var checkBox = document.getElementById("myCheck");
+// Get the modal
+var modal = document.getElementById("my-modal");
+// Get the <span> element that closes the modal
+var span = document.getElementById("close-span");
+// Get the checkbox
+var checkBox = document.getElementById("myCheck");
 
+checkBox.addEventListener("click",myFunction,false);
+  
+function myFunction() {
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
-    window.alert("Cambiando a modo Médico, por favor espere...");
-    setTimeout(function(){window.location.href="HomeMedico.html";},1000);
+    document.getElementById("text-head").innerHTML="ATENCIÓN";
+    document.getElementById("text-modal").innerHTML ="Cambiando a modo Médico, por favor espere...";
+    //The modal is display
+    modal.style.display = "block";
+    setTimeout(function(){window.location.href="HomeMedico.html";},2000);
   } else {
-    window.alert("Cambiando a modo Paciente, por favor espere...");
-    setTimeout(function(){window.location.href="Home.html";},1000);
+    document.getElementById("text-head").innerHTML="ATENCIÓN";
+    document.getElementById("text-modal").innerHTML ="Cambiando a modo Paciente, por favor espere...";
+    //The modal is display
+    modal.style.display = "block";
+    setTimeout(function(){window.location.href="index.html";},2000);
   }
 }
+
+span.addEventListener("click", e => {
+  e.preventDefault();
+  modal.style.display = "none";
+})
