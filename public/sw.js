c16941f554d7615c2d30e5844d0b536c4b381021
Gloria-2001 <olimen141@gmail.com>
2021-06-15 16:24:32 -0500

    Modificacion Service Worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const staticCacheName='site-static-v0';
-const dynamicCacheName='site-dynamic-v2';
+const dynamicCacheName='site-dynamic-v0';
 const assets=[
     './',
     './index.html',
@@ -85,19 +85,21 @@ self.addEventListener("activate",(evt)=>{
 //fetch event 
 self.addEventListener("fetch",(evt)=>{
     //console.log("Fetch event",evt);
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes=>{
-            return cacheRes || fetch(evt.request).then(fetchRes=>{
-                return caches.open(dynamicCacheName).then(cache=>{
-                    cache.put(evt.request.url,fetchRes.clone());
-                    limitCacheSize(dynamicCacheName,400)
-                    return fetchRes;
+    if (evt.request.url.indexOf('https://databases-auth.000webhost.com') === -1) {
+        evt.respondWith(
+                caches.match(evt.request).then(cacheRes=>{
+                    return cacheRes || fetch(evt.request).then(fetchRes=>{
+                        return caches.open(dynamicCacheName).then(cache=>{
+                            cache.put(evt.request.url,fetchRes.clone());
+                            limitCacheSize(dynamicCacheName,400)
+                            return fetchRes;
+                        })
+                    });
+                }).catch(()=> {
+                    if(evt.request.url.indexOf('.html') > -1){
+                        return caches.match('./fallback.html');
+                    }
                 })
-            });
-        }).catch(()=> {
-            if(evt.request.url.indexOf('.html') > -1){
-                return caches.match('./fallback.html');
-            }
-        })
-    );
+        );
+    }
 });
