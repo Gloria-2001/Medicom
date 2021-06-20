ef3938c7897c729208875a49f80e121fbd3de6a8
pablo-morales-01 <pmoralesz1600@alumno.ipn.mx>
2021-06-20 00:37:27 -0500

    Actualizacion enviarCorreo

---
+++ public_html/enviarCorreo.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'pablomorzafebrero@hotmail.com';
+$asunto = 'Mensaje de MediCom';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
